11a4b3dc0318ed39db4f0e7c9a2c5b20e97f6648
Anatoly Trishin <trishinanv@gmail.com>
2022-09-28 22:23:41 +0300

    Коммит класса Book

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    String name;
+    int pageCount;
+    int year;
+    String creator;
+
+    //конструктор с полями
+    Book(String name, int pageCount, int year, String creator) {
+        this.name = name;
+        this.pageCount = pageCount;
+        this.year = year;
+        this.creator = creator;
+    }
+
+    Book() {
+
+    }
+
+    //метод get
+    String getName() {
+        return name;
+    }
+
+    //метод set
+    void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Book firstBook = new Book("Три мушкетера", 672, 1844, "Дюма");
+        System.out.println(firstBook.getName());
+        System.out.println(firstBook.creator);
+
+        Book secondBook = new Book();
+        secondBook.setName("новая книга");
+        System.out.println(secondBook.getName());
+
+
+    }
+}
+
+
