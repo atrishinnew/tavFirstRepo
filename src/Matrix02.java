4c23c99a485598520412e4bbcffaf76e3c9befe6
Anatoly Trishin <trishinanv@gmail.com>
2022-10-10 21:19:03 +0300

    Переписал пока без тестов. надеюсь стало лучше )

--- src/Matrix02.java
+++ src/Matrix02.java
@@ -1,64 +1,162 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Matrix02 {
-    static int n,m;
-    int [][] array = new int[n][m];
+    int[][] array;
 
-    Matrix02(int[][] array) {
-       this.array = array;
+    //пустой конструктор
+    Matrix02 () {
+
     }
 
-    //метод, который установит размерность матрицы
-    static void setDimension () {
-        Scanner console = new Scanner(System.in);
-        System.out.println("Введите кол-во строк: ");
-        n = console.nextInt();
-        System.out.println("Введите кол-во столбцов: ");
-        m = console.nextInt();
+    //конструктор с безразмерным массивом array
+    Matrix02 (int[][] array) {
+        this.array = array;
     }
 
-    //метод заполнения матрицы мусором
-    void setMatrix() {
+    //конструктор с возможностью указать размер массива
+    Matrix02 (int n, int m) {
+        this.array = new int [n][m];
+    }
 
-        for (int i = 0; i < this.array.length; i++) {
-            for (int j = 0; j < this.array[0].length; j++) {
-                this.array[i][j] = j;
+    //просто метод, на случай, если хочу заполнить чем-то массив
+    static int[][] setArray(int n, int m) {
+        int [][] array = new int[n][m];
+        for (int i=0; i < n; i++) {
+            for (int j=0; j<m; j++) {
+                array[i][j]= i + j;
             }
         }
+        return array;
     }
 
-    public static void main (String[] args) {
+    // метод для получения матрицы
+    // (считаю, что под получением здесь имеется в виду создание нового объекта "матрица")
+    // например, я создал объект с пустым конструктором и уже этим методом задал ей размерность
+    // иначе пока не понятно, зачем такое придумывать, если можно воспользоваться "размерным" конструктором
+    int[][] getArray(int n, int m) {
+        this.array = new int[n][m];
+        return this.array;
+    }
 
-        //устанавливаем размерность матрицы
-        setDimension();
-        int [][] array1 = new int[n][m];
+    // метод получения  количество строк
+    int getN(int[][] matrix) {
+        return matrix.length;
 
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array1[0].length; j++) {
-                array1[i][j] = j;
+    }
+
+    //получить количество столбцов
+    int getM(int[][] matrix) {
+        return matrix[0].length;
+    }
+
+    //получить выбранный элемент
+    int getValue(int n, int m) {
+        return array[n][m];
+    }
+
+    //задать значение определенному элементу
+    void setValue(int n, int m, int value) {
+        this.array[n][m] = value;
+    }
+
+    //сложение массивов
+    int[][] sumMatrix (int[][] matrix1, int[][] matrix2) {
+        for (int i=0; i < getN(matrix1); i++){
+            for (int j=0; j < getM(matrix1); j++) {
+                this.array[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
-        //чтобы при создании объекта класса Матрица отдать на вход массив, его где-то надо взять
-        //но до создания объекта, у меня еще нет массива (так как массив это поле класса)
-        //поэтому выше в main я вынужден еще создать "локальный" массив
-        Matrix02 firstMatrix = new Matrix02(array1);
-        //заполняем матрицу мусором
-        //firstMatrix.setMatrix();
+        return this.array;
 
-        //setDimension();
-        //int [][] array2 = new int[n][m];
-        //Matrix02 secondMatrix = new Matrix02(array2);
-        //заполняем матрицу мусором
-        //secondMatrix.setMatrix();
+    }
 
+    //перемножение массивов
+    int [][] multiMatrix (int[][] matrix1, int[][] matrix2) {
+
+        for (int coll = 0; coll < getM(matrix2); coll++) {
+
+            for (int i = 0; i < getN(matrix1); i++) {
+                int tempVar = 0;
+                for (int j = 0; j < getM(matrix2); j++) {
+                    tempVar += (matrix1[i][j] * matrix2[j][coll]);
+                }
+                this.array[i][coll] = tempVar;
+            }
+        }
+        return this.array;
+    }
+
+
+public static void main (String[] args) {
+
+
+        //int[][] localArray = new int [][] {{1,2,3},{5,6,7}};
+
+        /*int[][] localArray = new int [n][m];
+        localArray = setArray(5,6);*/
+
+        // ===== первый вариант создания матрицы
+        int[][] firstArray = setArray(3,3);
+        int[][] secondArray;
+
+        Matrix02 firstM = new Matrix02(firstArray);
+
+        // ===== второй вариант создания матрицы
+        Matrix02 secondM = new Matrix02(new int [][] {{1,2,3},{5,6,7},{5,6,7}});
+
+        // ===== третий вариант создания матрицы
+
+        Matrix02 thirdM = new Matrix02(5,7);
+
+        // ==== четвертый вариант создания матрицы
+        Matrix02 fourthM = new Matrix02();
+        fourthM.getArray(3,2);
+
+        Matrix02 fifthM = new Matrix02(3,3);
+
+        // ==== вывод в консоль содержимого матриц разных объектов
+        System.out.println(Arrays.deepToString(firstM.array));
+
+        System.out.println(Arrays.deepToString(secondM.array));
+
+        System.out.println(Arrays.deepToString(thirdM.array));
+
+        System.out.println(Arrays.deepToString(fourthM.array));
+
+        // ==== получить количество строк N матрицы и вывод на печать
+        System.out.println(firstM.getN(secondM.array));
+
+        // ==== получить количество столбцов M матрицы и вывод на печать
+        System.out.println(firstM.getM(secondM.array));
+
+        // ==== получить определенный элемент матрицы
+        System.out.println(firstM.getValue(1,1));
+        System.out.println(secondM.getValue(1,1));
+
+        // ==== задать значение определенному элементу матрицы
+        firstM.setValue(1,1, 5);
+        firstM.setValue(2,2, 3);
+        System.out.println(firstM.array[1][1]);
+        System.out.println(secondM.array[1][1]);
         System.out.println("=====");
 
-        System.out.println(Arrays.deepToString(firstMatrix.array));
-        //тут я не пойму, почему "локальный" массив array1 также заполняется, как и firstMatrix.array???
-        System.out.println(Arrays.deepToString(array1));
-        //System.out.println(Arrays.deepToString(secondMatrix.array));
-        //System.out.println(Arrays.deepToString(firstMatrix.array));
-    }
+        // ==== сложить матрицы
+
+        fifthM.sumMatrix(secondM.array,secondM.array);
+        System.out.println("=====");
+        System.out.println(Arrays.deepToString(fifthM.array));
+
+        // ==== перемножить матрицы
+
+        fifthM.multiMatrix(secondM.array,secondM.array);
+        System.out.println("=====");
+        System.out.println(Arrays.deepToString(fifthM.array));
 
 }
+
+
+
+
+}
+
+
