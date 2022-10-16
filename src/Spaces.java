7ba3f5152ee9ff1ce704d70a3a3a5fab6796d103
Anatoly Trishin <trishinanv@gmail.com>
2022-10-16 17:38:13 +0300

    Программа, которая подсчитывает кол-во пробелов в фразе, которую введет пользователь. Написал тест.

---
+++ src/Spaces.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+/**
+ * Класс, подсчитывающий пробелы в тексте
+ */
+
+public class Spaces {
+
+    String text;
+    Spaces (String text) {
+        this.text = text;
+    }
+
+    //метод (статичный) считывания текста из консоли
+    static String getInputText () {
+        String text;
+        Scanner console = new Scanner(System.in);
+        text = console.nextLine();
+        return text;
+
+    }
+
+    //метод класса перевода текста в массив
+    public char[] convertToArray (String text) {
+        return text.toCharArray();
+    }
+
+    //метод класса подсчета количества пробелов
+    /*int doCountSpaces(char[] chArray) {
+        int count = 0;
+        for (int i=0; i<chArray.length; i++) {
+            if (chArray[i] == ' ') {
+                count ++;
+            }
+        }
+        return count;
+    }*/
+    int doCountSpaces(char[] chArray) {
+        int count = 0;
+        for (char symbol : chArray) {
+            if (symbol == ' ') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Введите текст");
+        Spaces newSpaces = new Spaces(getInputText());
+        System.out.println("Количество пробелов: " + newSpaces.doCountSpaces(newSpaces.convertToArray(newSpaces.text)));
+
+    }
+
+
+}
---
+++ src/SpacesTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SpacesTest {
+    @Test
+    public void test () {
+        Spaces spTest = new Spaces("a b c");
+        assertEquals(2, spTest.doCountSpaces(spTest.convertToArray(spTest.text)));
+    }
+
+}
